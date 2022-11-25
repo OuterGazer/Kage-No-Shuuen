024f4c61432b87996f0909f03bf905b7e536fb56
OuterGazer <aythami.cabrera@gmail.com>
2022-11-25 10:04:04 +0100

    Changed Input and Movement scripts execution order to have input always go first. Implemented character always looking forward on movement, so player can see rotate camera to see character's face when still.

--- Assets/Characters/Scripts/CharacterMovement.cs
+++ Assets/Characters/Scripts/CharacterMovement.cs
@@ -24,14 +24,25 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateCharacterMovement();
+        UpdateHorizontalMovement();
     }
 
-    private void UpdateCharacterMovement()
+    private void UpdateHorizontalMovement()
     {
         Vector3 movement = ApplyMovementRelativeToCameraPosition();
 
         characterController.Move(movement * speed * Time.deltaTime);
+
+        MakeCharacterAlwaysFaceForwardOnMovement(movement);
+    }
+
+    private void MakeCharacterAlwaysFaceForwardOnMovement(Vector3 movement)
+    {
+        if (movement != Vector3.zero)
+        {
+            Vector3 projectedForwardVector = Vector3.ProjectOnPlane(mainCamera.transform.forward, Vector3.up);
+            transform.forward = projectedForwardVector;
+        }
     }
 
     private Vector3 ApplyMovementRelativeToCameraPosition()
