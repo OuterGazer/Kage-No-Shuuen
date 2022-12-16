2524821a3aae242bc8da0a0542c307a0f3bec5fd
OuterGazer <aythami.cabrera@gmail.com>
2022-12-16 15:38:43 +0100

    Fixed throwing animation and rig. Now character rotates towards target and arm aims towards it, too. TODO: try to implement landing animation.

--- Assets/Characters/Scripts/CharacterOnHookState.cs
+++ Assets/Characters/Scripts/CharacterOnHookState.cs
@@ -18,13 +18,14 @@ public class CharacterOnHookState : CharacterMovementBase
     [HideInInspector] public UnityEvent throwHook;
     [HideInInspector] public UnityEvent<bool> changeToHangingAnimation;
     private CharacterAnimator characterAnimator;
-    //private ChainIKConstraint hookHandConstraint;
+    private ChainIKConstraint hookHandConstraint;
 
     private Vector3 hangingDirection;
     public Vector3 HangingDirection => hangingDirection;
 
     private float currentOnHookSpeed;
-    
+
+    private bool isHookThrown = false;
 
     private void Awake()
     {
@@ -32,30 +33,59 @@ public class CharacterOnHookState : CharacterMovementBase
 
         characterAnimator = GetComponent<CharacterAnimator>();
         characterAnimator.hookHasArrivedAtTarget.AddListener(MoveCharacterToHookTarget);
-        //hookHandConstraint = GetComponentInChildren<ChainIKConstraint>();
-        //hookHandConstraint.weight = 0f;
+        hookHandConstraint = GetComponentInChildren<ChainIKConstraint>();
+        hookHandConstraint.weight = 0f;
     }
 
     private void OnEnable()
     {
+        transform.forward = Vector3.ProjectOnPlane(hookTarget.position, Vector3.up);
+
         throwHook.Invoke();
-        //hookHandConstraint.weight= 1.0f;
+        
+        isHookThrown = false;
         currentOnHookSpeed = 0f;
         hangingDirection = Vector3.zero;
     }
 
     private void Update()
     {
+        PointArmTowardsHookTarget();
+
         charController.Move(currentOnHookSpeed * Time.deltaTime * hangingDirection);
 
-        if ((hookTarget.position - transform.position).sqrMagnitude <= hookReachThreshold)
-        {
-            changeToHangingAnimation.Invoke(false);
 
-            onAirState.enabled = true;
-            //hookHandConstraint.weight = 0f;
-            this.enabled = false;
+        if (IsHookTargetReached())
+        {
+            ExitState();
         }
+    }
+
+    private void PointArmTowardsHookTarget()
+    {
+        if (hookHandConstraint.weight < 1f && !isHookThrown)
+        {
+            hookHandConstraint.weight += 0.01f;
+        }
+        else
+        {
+            isHookThrown = true;
+            hookHandConstraint.weight = 0f;
+        }
+    }
+
+    private bool IsHookTargetReached()
+    {
+        return (hookTarget.position - transform.position).sqrMagnitude <= hookReachThreshold;
+    }
+
+    private void ExitState()
+    {
+        changeToHangingAnimation.Invoke(false);
+
+        onAirState.enabled = true;
+        hookHandConstraint.weight = 0f;
+        this.enabled = false;
     }
 
     public void MoveCharacterToHookTarget()
@@ -63,7 +93,7 @@ public class CharacterOnHookState : CharacterMovementBase
         hangingDirection = (hookTarget.position - transform.position).normalized;
         transform.up = hangingDirection;
         currentOnHookSpeed = speed;
-        //hookHandConstraint.weight = 0f;
+        hookHandConstraint.weight = 0f;
         changeToHangingAnimation.Invoke(true);
     }
 }
