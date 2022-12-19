b26b57189c37df24447dfc9ffcef9df7ee5b1e40
OuterGazer <aythami.cabrera@gmail.com>
2022-12-19 17:21:23 +0100

    Finished implementing dodging, but need to refine it a bit.

--- Assets/Characters/Scripts/CharacterDodgingState.cs
+++ Assets/Characters/Scripts/CharacterDodgingState.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(CharacterIdleState))]
 public class CharacterDodgingState : CharacterMovementBase
 {
+    [SerializeField] float impulseThreshold = 3f;
+
     [Header("Exit States")]
     [SerializeField] CharacterIdleState idleState;
 
     [HideInInspector] public UnityEvent makeCharaterDodge;
 
     private Vector3 dodgeFacingDirection;
+    private float currentSpeed;
 
     private void Awake()
     {
@@ -21,14 +25,35 @@ public class CharacterDodgingState : CharacterMovementBase
     private void OnEnable()
     {
         makeCharaterDodge.Invoke();
+        currentSpeed = speed;
     }
 
     private void Update()
     {
         transform.forward = dodgeFacingDirection;
+
+        charController.Move(currentSpeed * Time.deltaTime * dodgeFacingDirection);
+
+        EaseOutCurrentSpeed();
     }
 
-    public void SetDodgeFacingDirection(Vector3 facingDirection)
+    private void EaseOutCurrentSpeed()
+    {
+        if (currentSpeed >= impulseThreshold)
+            currentSpeed = Mathf.Lerp(speed, 0f, EaseOutQuad(1 - (currentSpeed * 0.999f) / speed) * Time.deltaTime);
+        else
+            currentSpeed = Mathf.Lerp(speed, 0f, t -= 0.05f * Time.deltaTime);
+
+        Debug.Log(currentSpeed);
+    }
+
+    private float t;
+    private float EaseOutQuad(float x) 
+    {
+        return t = 1 - (1 - x) * (1 - x);
+    }
+
+public void SetDodgeFacingDirection(Vector3 facingDirection)
     {
         dodgeFacingDirection = facingDirection;
     }
