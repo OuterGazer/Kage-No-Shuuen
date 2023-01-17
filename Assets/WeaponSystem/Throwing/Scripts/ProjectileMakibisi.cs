cc8d64314f726de10a6e6c27bd1469e06a4b5883
OuterGazer <aythami.cabrera@gmail.com>
2023-01-17 11:06:08 +0100

    Added lifespan to thrown makibisis so they disappear after a set amount of time.

--- Assets/WeaponSystem/Throwing/Scripts/ProjectileMakibisi.cs
+++ Assets/WeaponSystem/Throwing/Scripts/ProjectileMakibisi.cs
@@ -9,15 +9,18 @@ public class ProjectileMakibisi : ProjectileBase
     [SerializeField] int amountThrown = 10;
     [SerializeField] float maxForce, minForce = 1f;
     [SerializeField] float maxAngle, minAngle = 1f;
+    [SerializeField] float lifeSpan = 5f;
 
+
+    // TODO: implement an Object Pool for this
     private void OnEnable()
     {
         GameObject[] thrownMakibisis = new GameObject[amountThrown];
 
         for(int i = 0; i < thrownMakibisis.Length; i++)
         {
-            GameObject current = thrownMakibisis[i];
-            current = Instantiate(extraMakibisiPrefab, transform.position, transform.rotation);
+            GameObject current = Instantiate(extraMakibisiPrefab, transform.position, transform.rotation);
+            thrownMakibisis[i] = current;
 
             float throwingStrength = Random.Range(maxForce, minForce);
             float throwingAngle = Random.Range(maxAngle, minAngle);
@@ -26,5 +29,19 @@ public class ProjectileMakibisi : ProjectileBase
             current.transform.localRotation *= Quaternion.Euler(0f, throwingAngle, 0f);
             tempRB.AddForce(current.transform.forward * throwingStrength, ForceMode.Impulse);
         }
+
+        StartCoroutine(DisableThrownMakibisis(thrownMakibisis));
+    }
+
+    private IEnumerator DisableThrownMakibisis(GameObject[] thrownMakibisis)
+    {
+        yield return new WaitForSeconds(lifeSpan);
+
+        foreach(GameObject item in thrownMakibisis)
+        {
+            Destroy(item);
+        }
+
+        Destroy(this.gameObject);
     }
 }
