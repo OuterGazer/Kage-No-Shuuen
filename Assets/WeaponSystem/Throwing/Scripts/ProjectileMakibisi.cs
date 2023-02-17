7cfa3f47b90c3280b38289fb14155952216fd621
OuterGazer <aythami.cabrera@gmail.com>
2023-02-17 12:16:02 +0100

    Fixed Makibisi bug that made them have an unexpected behaviour upon throwing.

--- Assets/WeaponSystem/Throwing/Scripts/ProjectileMakibisi.cs
+++ Assets/WeaponSystem/Throwing/Scripts/ProjectileMakibisi.cs
@@ -11,17 +11,20 @@ public class ProjectileMakibisi : ProjectileBase
     [SerializeField] float maxAngle, minAngle = 1f;
     [SerializeField] float lifeSpan = 5f;
 
+    Collider[] colliders = new Collider[50];
 
-    // TODO: implement an Object Pool for this
+    // TODO: implement an Object Pool for this and clean the collider disabling/enabling
     private void OnEnable()
     {
         GameObject[] thrownMakibisis = new GameObject[amountThrown];
 
         for(int i = 0; i < thrownMakibisis.Length; i++)
         {
-            GameObject current = Instantiate(extraMakibisiPrefab, transform.position, transform.rotation);            
+            GameObject current = Instantiate(extraMakibisiPrefab, transform.position, transform.rotation);
             thrownMakibisis[i] = current;
-            thrownMakibisis[i].GetComponent<ProjectileBase>().SetOwnerTag(tag);
+            
+            colliders[i] = thrownMakibisis[i].GetComponentInChildren<Collider>(); // Need to do this so makibisis don't collide among themselves and create buggy behaviour
+            colliders[i].enabled = false;
 
             float throwingStrength = Random.Range(maxForce, minForce);
             float throwingAngle = Random.Range(maxAngle, minAngle);
@@ -36,6 +39,13 @@ public class ProjectileMakibisi : ProjectileBase
 
     private IEnumerator DisableThrownMakibisis(GameObject[] thrownMakibisis)
     {
+        yield return new WaitForSeconds(0.4f);
+
+        foreach(Collider item in colliders)
+        {
+            item.enabled = true;
+        }
+
         yield return new WaitForSeconds(lifeSpan);
 
         foreach(GameObject item in thrownMakibisis)
