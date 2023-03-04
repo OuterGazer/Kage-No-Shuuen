4c5e15aedaabae3aaefdc8c50e7049c56929d6b1
OuterGazer <aythami.cabrera@gmail.com>
2023-03-04 11:54:26 +0100

    Implemented stealth kill victim animation.

--- Assets/Characters/Scripts/CombatStates/CharacterStealthKillState.cs
+++ Assets/Characters/Scripts/CombatStates/CharacterStealthKillState.cs
@@ -5,10 +5,19 @@ using UnityEngine.Events;
 
 public class CharacterStealthKillState : CharacterStateBase
 {
+    [SerializeField] LayerMask targetLayerMask = Physics.DefaultRaycastLayers;
+
     [HideInInspector] public UnityEvent onStealthKill;
+
+    private float stealthKillRadius = 3f;
 
     private void OnEnable()
     {
+        Collider[] targets = Physics.OverlapSphere(transform.position, stealthKillRadius, targetLayerMask);
+
+        CharacterAnimator targetAnimator = targets[0].GetComponentInParent<CharacterAnimator>();
+
         onStealthKill.Invoke();
+        targetAnimator?.PlayStealthDeathAnimation();
     }
 }
